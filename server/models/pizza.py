b57ea26b51fa90987d7988abdc0f2bbf43f51563
petermtgi <3sjk0krrr@gmail.com>
2025-06-17 20:41:17 +0300

    update

---
+++ server/models/pizza.py
@@ -0,0 +1,11 @@
+from server.config import db
+from sqlalchemy.orm import relationship
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    ingredients = db.Column(db.String, nullable=False)
+
+    restaurant_pizzas = relationship('RestaurantPizza', backref='pizza')
